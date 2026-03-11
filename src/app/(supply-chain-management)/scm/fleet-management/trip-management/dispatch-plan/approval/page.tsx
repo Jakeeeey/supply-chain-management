43d504cfd4be3b390dec6951201ab5cb8d973c59
Admin <ajbsiapno@gmail.com>
2026-03-11 13:06:02 +0800

    Consolidation Workflow

--- src/app/(supply-chain-management)/scm/fleet-management/trip-management/dispatch-plan/approval/page.tsx
+++ src/app/(supply-chain-management)/scm/fleet-management/trip-management/dispatch-plan/approval/page.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { NavUser } from "../../../_components/nav-user";
+import { NavUser } from "../../../../_components/nav-user";
 
 import { cookies } from "next/headers";
 
