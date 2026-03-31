afb20424bc51566cf2a77fb46d73fcbef537a1f4
Jereco Peralta <peraltajereco@gmail.com>
2026-03-31 08:59:00 +0800

    changes from the next config ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< Updated upstream
-  allowedDevOrigins: ["msi-4", "kyaaah"],
-=======
-  allowedDevOrigins: ['msi-4', '192.168.1.239', '100.81.225.79', 'localhost'],
->>>>>>> Stashed changes
+  allowedDevOrigins: ["msi-4", "kyaaah", "192.168.1.239", "100.81.225.79", "localhost"],
 };
 
 export default nextConfig;
