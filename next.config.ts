94586b80540f1b018cf3c0e0eb7a4b964cc3fa05
zernanvash <zernanvasharive16@gmail.com>
2026-07-07 17:33:10 +0800

    chore: remove package.json and next.config.ts from feature branch

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ["100.81.225.79", "msi-eulysis", "100.116.150.68"],
-};
-
-export default nextConfig;
